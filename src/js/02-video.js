8688ab0a9e4fba27da83434405fe8b3777b23127
Tymofii-Shtyrba <timofeyshtyrba@gmail.com>
2023-03-05 15:42:48 +0200

    Done the second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const timestamp = localStorage.getItem("timestamp");
+if (timestamp !== null) {
+  player.setCurrentTime(timestamp);
+}
+
+const callback = data => localStorage.setItem("timestamp", String(data.seconds));
+
+player.on('timeupdate', throttle(callback, 1000));
 
